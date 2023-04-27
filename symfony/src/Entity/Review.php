4fe48d1bcf15ae4e14c23b96b04111a210cc5c51
omarbouraoui01 <bouraoui.omar@esprit.tn>
2023-04-27 14:55:13 +0200

    crud+reponse+ban

---
+++ symfony/src/Entity/Review.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Review
+ *
+ * @ORM\Table(name="review", indexes={@ORM\Index(name="fku2", columns={"id_user"}), @ORM\Index(name="fku1", columns={"id_editeur"})})
+ * @ORM\Entity
+ */
+class Review
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="string", length=500, nullable=false)
+     */
+    private $message;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="note", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $note;
+
+    /**
+     * @var \Utilisateur
+     *
+     * @ORM\ManyToOne(targetEntity="Utilisateur")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_editeur", referencedColumnName="id")
+     * })
+     */
+    private $idEditeur;
+
+    /**
+     * @var \Utilisateur
+     *
+     * @ORM\ManyToOne(targetEntity="Utilisateur")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_user", referencedColumnName="id")
+     * })
+     */
+    private $idUser;
+
+
+}
